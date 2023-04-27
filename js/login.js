504afe4ead2c9037dbc5e45e9af58c62bd04893e
Dário Batista <dariolbat@hotmail.com>
2023-04-27 17:30:06 +0100

    adicionado dados de login

---
+++ js/login.js
@@ -0,0 +1,13 @@
+// Variaveis
+
+const USER_EMAIL_KEY = "bejewel_user_email"
+const USER_PASSWORD_KEY  = "bejewel_user_password"
+
+function saveLogin()
+{
+    let user_email      = login_form.elements.email.value;
+    let user_password   = login_form.elements.senha.value;
+
+    localStorage.setItem(USER_EMAIL_KEY, user_email);
+    localStorage.setItem(USER_PASSWORD_KEY, user_password);
+}
